7d8e536ec3345bd72e75d74f0113307a83b27af3
Prince Kumar <165800173+rover-7-7@users.noreply.github.com>
2024-10-13 12:04:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 
 let userMessage = null; // Variable to store user's message
-const API_KEY = "sk-proj-K8GBT5bpJeVkwvwenTlCT3BlbkFJjhHH1jSwmujbAbCHDO5n";
+const API_KEY = "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";
 const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
